8e7bde54a12bfd8ff63eeb466c84974f6a98cdaf
ipavlakos <pavlakosIlias@outlook.com>
2025-03-05 14:58:45 +0200

    Request objects and query params

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,10 +11,25 @@ Route::get('/jobs', function () {
     return 'Available jobs!';
 })->name('jobs');
 
-Route::get('/posts/{id}', function (string $id) {
-   return 'Post ' . $id;
-})->where('id', '[0-9]+');
+Route::get('/test', function (Request $request) {
+    return [
+        'method' => request()->method(),
+        'uri' => $request->path(),
+        'ip' => $request->ip(),
+        'path' => $request->path(),
+        'userAgent' => $request->userAgent(),
+        'header' => $request->header(),
+    ];
+});
 
-Route::get('/posts/{id}/comments/{commentId}', function (string $id, string $commentId) {
-    return 'Post ' . $id . ' comment ' . $commentId;
+Route::get('/users', function (Request $request) {
+    return $request->all(['name', 'age']);
+});
+
+Route::get('/users2', function (Request $request) {
+    return $request->all();
+});
+
+Route::get('/users3', function (Request $request) {
+    return $request->has('name');
 });
