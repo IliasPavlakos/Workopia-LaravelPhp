ae6b3b61d6779041fcb283eecc9595ec6b150461
ipavlakos <pavlakosIlias@outlook.com>
2025-03-05 15:23:16 +0200

    Creating and displaying views

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,12 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    return 'Available jobs!';
+    return view('jobs.index');
 })->name('jobs');
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
+})->name('jobs.create');
 
 Route::get('/test', function (Request $request) {
     return [
@@ -21,28 +25,4 @@ Route::get('/test', function (Request $request) {
         'userAgent' => $request->userAgent(),
         'header' => $request->header(),
     ];
-});
-
-Route::get('/test2', function () {
-    return response('Hello World!', 200);
-});
-
-Route::get('/test3', function () {
-    return new Response('Hello World!', 200);
-});
-
-Route::get('/test4', function () {
-    return response('<h1>Hello World!</h1>', 200)->header('content-type', 'text/html');
-});
-
-Route::get('/test5', function () {
-    return response('<h1>Hello World!</h1>', 200)->header('content-type', 'text/plain');
-});
-
-Route::get('/download', function () {
-    return response()->download(public_path('favicon.ico'));
-});
-
-Route::get('/test6', function () {
-    return response('<h1>Hello World!</h1>', 200)->header('content-type', 'text/plain')->cookie('name', 'ilias');
 });
