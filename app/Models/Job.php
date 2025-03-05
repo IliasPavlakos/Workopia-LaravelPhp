a19e87eabe601ee57ec86348962c564d4be5911c
ipavlakos <pavlakosIlias@outlook.com>
2025-03-05 20:44:39 +0200

    Migration with Eloquent relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Job extends Model
 {
@@ -11,8 +12,14 @@ class Job extends Model
 
     protected $table = 'job_listings';
 
-    protected $fillable = [ 'title', 'description', 'salary', 'tags', 'type', 'remote',
+    protected $fillable = [ 'user_id', 'title', 'description', 'salary', 'tags', 'type', 'remote',
         'requirements', 'benefits', 'city', 'state', 'zipcode',
         'contact_email', 'contact_phone', 'company_name', 'company_description',
         'company_logo', 'company_website' ];
+
+    // Relation to user
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2025_03_05_182631_add_fields_to_job_listings_table.php
+++ database/migrations/2025_03_05_182631_add_fields_to_job_listings_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,8 +14,7 @@ return new class extends Migration
         DB::table('job_listings')->truncate();
 
         Schema::table('job_listings', function (Blueprint $table) {
-            $table->string('title');
-            $table->string('description');
+            $table->unsignedInteger('user_id');
             $table->integer('salary');
             $table->string('tags')->nullable();
             $table->enum('type', ['Full-Time', 'Part-Time', 'Contract', 'Internship'])->default('Full-Time');
@@ -32,7 +30,11 @@ return new class extends Migration
             $table->string('company_description')->nullable();
             $table->string('company_logo');
             $table->string('company_website')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
+
+
     }
 
     /**
@@ -41,12 +43,13 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('job_listings', function (Blueprint $table) {
-            $table->dropColumn([ 'title', 'description',
+            $table->dropForeign(['user_id']);
+            $table->dropColumn([ 'user_id',
                 'salary', 'tags', 'type', 'remote',
                 'requirements', 'benefits', 'city', 'state', 'zipcode',
                 'contact_email', 'contact_phone', 'company_name', 'company_description',
                 'company_logo', 'company_website'
-                ]);
+            ]);
         });
     }
 };
