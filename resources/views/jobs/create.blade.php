8aafc6c3776c313800c7d23b2afad52737ab60d6
ipavlakos <pavlakosIlias@outlook.com>
2025-03-05 16:14:00 +0200

    Layouts with template inheritance

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -1,18 +1,17 @@
+@extends('layout')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Job</title>
-</head>
-<body>
-<h1>Create new job</h1>
-<form action="/jobs" method="POST">
-    @csrf
-    <input type="text" name="title" placeholder="title" />
-    <input type="text" name="description" placeholder="description" />
-    <button type="submit">Submit</button>
-</form>
-</body>
-</html>
+@section('title')
+    Create Job
+@endsection
+
+@section('content')
+
+    <h1>Create new job</h1>
+    <form action="/jobs" method="POST">
+        @csrf
+        <input type="text" name="title" placeholder="title"/>
+        <input type="text" name="description" placeholder="description"/>
+        <button type="submit">Submit</button>
+    </form>
+
+@endsection
--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,24 +1,16 @@
-<?php
+@extends('layout')
 
-?>
+@section('content')
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Job Listings</title>
-</head>
-<body>
-<h1>Available Jobs</h1>
-@if(!empty($jobs))
-    <ul>
-        @foreach($jobs as $job)
-            <li>{{$job}}</li>
-        @endforeach
-    </ul>
-@else
-    <p>No jobs available!</p>
-@endif
-</body>
-</html>
+    <h1>Available Jobs</h1>
+    @if(!empty($jobs))
+        <ul>
+            @foreach($jobs as $job)
+                <li>{{$job}}</li>
+            @endforeach
+        </ul>
+    @else
+        <p>No jobs available!</p>
+    @endif
+
+@endsection
