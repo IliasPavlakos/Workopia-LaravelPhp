94dc72872c6d222e8f7d0637ac4794dcfb28e195
ipavlakos <pavlakosIlias@outlook.com>
2025-03-05 21:16:06 +0200

    Creating and using factories

---
+++ database/factories/JobFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Job>
+ */
+class JobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->jobTitle(),
+            'description' => $this->faker->paragraphs(1, true),
+            'salary' => $this->faker->numberBetween(1000, 10000),
+            'tags' => implode(',', $this->faker->words(3)),
+            'type' => $this->faker->randomElement(['Full-Time', 'Part-Time', 'Contract', 'Internship']),
+            'remote' => $this->faker->boolean(),
+            'requirements' => $this->faker->sentences(3, true),
+            'benefits' => $this->faker->sentences(2, true),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->word(),
+            'zipcode' => $this->faker->postcode(),
+            'contact_email' => $this->faker->email(),
+            'contact_phone' => $this->faker->phoneNumber(),
+            'company_name' => $this->faker->company(),
+            'company_description' => $this->faker->paragraphs(1, true),
+            'company_logo' => $this->faker->imageUrl(100, 100, 'business', true, 'logo'),
+            'company_website' => $this->faker->url(),
+        ];
+    }
+}
