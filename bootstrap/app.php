7269cd308e7a579bd52ff60048ef0be06d6ad718
ipavlakos <pavlakosIlias@outlook.com>
2025-03-05 14:45:52 +0200

    Creating routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except: ['/submit']); // bypasses the csrf token validation for the /submit route
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return 'Available jobs!';
+})->name('jobs');
+
+Route::post('/submit', function () {
+    return 'Submitted!';
+});
+
+Route::get('/test', function () {
+   $url = route('jobs');
+   return "<a href='$url'>Click here!</a>";
+});
+
+Route::get('/api/users', function () {
+   return [
+       'name' => 'John Doe',
+       'email' => 'john@doe.com',
+   ];
+});
